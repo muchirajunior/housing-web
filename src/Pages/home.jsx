8f273886ac9376640b276d9b10d02d4e2e69d5f8
muchirajunior <muchirajunior022@gmail.com>
2022-05-29 13:51:43 +0300

    added a nav bar

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { collection, getDocs } from "firebase/firestore";
 import { db, imgUrl } from '../firebase-config';
+import Navbar from '../components/navbar.';
 
 
 
@@ -19,11 +20,12 @@ function Home() {
 
   }, [0])
   return (
-    <div className='container'>
+    <div className='bg-light'>
+      < Navbar />
       <div className='row'>
 
         {houses.length > 1 ? houses.map((house) =>
-          <div className="card m-3" style={{ "width": "18rem" }}>
+          <div className="card m-3 border mx-auto" style={{ "width": "18rem" }}>
             <img src={imgUrl(house.image)} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{house.house}</h5>
